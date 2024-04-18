fb86f7484deb199ee8bbac0cf4dfc13489672734
cejimenez0123 <christianjimenez0123@gmail.com>
2024-04-18 14:57:32 -0400

    token

--- fitness-tracker-backend/index.js
+++ fitness-tracker-backend/index.js
@@ -1,14 +1,40 @@
-const express = require('express');
+const express = require("express");
+const session = require("express-session")
+const passport = require("passport")
 const bodyParser = require('body-parser');
 const dotenv = require("dotenv");
+const cors = require('cors')
 dotenv.config()
-const {setUpPassportLocal}= require("./middleware/authMiddleware.js")
+const {setUpPassportLocal}= require("./middleware/authMiddlleware.js")
 const app = express();
 const PORT = process.env.PORT || 3000
+const userRoutes = require('./routes/user.js')
+const authMiddleware = passport.authenticate('bearer', { session: false });
+app.use(cors())
+const logger = (req, _res, next) => {
+    const time = new Date().toLocaleTimeString();
+    console.log(`${time} ${req.method}: ${req.url}`);
+    next();
+    };
+    app.use("/user", userRoutes(authMiddleware))
+    app.get('/', (req, res, next) => {
+    
+        res.status(200).json({message:"Hello World"})
+    })
+setUpPassportLocal(passport);
+app.use(
+    session({
+    secret: process.env.JWT_SECRET,resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+    }))
+app.use(passport.session());
+app.use(passport.initialize());
+app.use(bodyParser.json());
+app.use(logger);
 
 
-
-
+app.use("/user", userRoutes(authMiddleware))
 
 
 
