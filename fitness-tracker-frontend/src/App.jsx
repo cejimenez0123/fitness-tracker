7769380ba67f24872452a3f8aca90b0b6ad756b5
Mahamad Jabby <jabby.mahamad@gmail.com>
2024-05-13 18:04:04 -0400

    first commit

--- fitness-tracker-frontend/src/App.jsx
+++ fitness-tracker-frontend/src/App.jsx
@@ -1,55 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Enviroment from './core'
-import axios from "axios"
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Enviroment from "./core";
+import axios from "axios";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
 function App() {
-  const [email,setEmail]=useState("")
-  const [password,setPassword]=useState("")
-  const handlePassword = (e)=>{
-    console.log(e.target.value)
-    setPassword(e.target.value)
-  }
-  const handleEmail = (e)=>{ 
-    console.log(e.target.value)
-    setEmail(e.target.value)
-  }
-  const handleRegister= (e)=>{
-    e.preventDefault()
-    axios({
-      method: 'post',
-      url: Enviroment.BASE_URL + "/user/register", 
-      data: {email:email,password:password,name:"Christian"}
-    }).then(res=>{
-      console.log(res.data)
-    })
-  }
-  const handleSubmit = (e)=>{
-    e.preventDefault()
-    axios({
-      method: 'post',
-      url: Enviroment.BASE_URL + "/user/login", 
-      data: {email:email,password:password}
-    }).then(res=>{
-      localStorage.setItem("token",res.data.token)
-      console.log(res.data)
-    })
-  }
   return (
-    <>
-      
-      <h1>Vite + React</h1>
-      <form onSubmit={(e)=>handleSubmit(e)} >
-        <input type="text" value={email} placeholder='email' name="email"
-          onChange={(e)=>handleEmail(e)}/>
-        <input type="text" value={password}placeholder="password" onChange={(e)=>handlePassword(e)}
-        name="password"/>
-        <button type='submit'>Submit</button>
-      </form>
-     
-    </>
-  )
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" component={hello} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
