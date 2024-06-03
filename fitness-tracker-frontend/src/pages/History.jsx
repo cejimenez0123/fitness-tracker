bf63b26990b0d85717afb4e44f6b3069525b7e2f
kehinde ojo <ojok761@gmail.com>
2024-06-03 10:03:57 -0400

    m

--- fitness-tracker-frontend/src/pages/History.jsx
+++ fitness-tracker-frontend/src/pages/History.jsx
@@ -7,6 +7,7 @@ if(isLoading){
   return(
 
     <div>Loading... </div>
+    
   )
 }
 if(isError){
@@ -16,10 +17,25 @@ if(isError){
   )
 }
   return (
-    <div>
-      hello this is going to be your history page 
-      
-    </div>
+    <>
+    <h1>Past workouts </h1>
+   
+    <div class="card w-96 bg-primary text-primary-content">
+  <div class="card-body">
+    <h2 class="card-title">Workout Name</h2>
+    <ul class="list-disc pl-4">  <li>
+        <span class="font-bold">Exercise:</span> Squats
+        <span class="ml-4">Sets:</span> 3
+        <span class="ml-4">Reps:</span> 12
+        <span class="ml-4">Weight:</span> 20 lbs
+        <span class="ml-4">Muscle:</span> Legs
+        <span class="ml-4">Type:</span> Compound
+      </li>
+      </ul>
+    <div class="card-footer text-muted">  </div>
+  </div>
+</div>
+    </>
   )
 }
 
