f863286e3e963eb7acff20ee3ebcb7e3399d9235
kehinde ojo <ojok761@gmail.com>
2024-06-04 23:17:51 -0400

    fea: New styles

--- fitness-tracker-frontend/src/pages/UserSignup.jsx
+++ fitness-tracker-frontend/src/pages/UserSignup.jsx
@@ -1,40 +1,42 @@
-import React from 'react'
-import axios from 'axios';
-import { useState } from 'react'
-import gym from "../../public/gym.jpg"
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+
+import gym from "../../public/gym.jpg";
+import { Link } from "react-router-dom";
 const UserSignup = () => {
   const [gender, setGender] = useState("male");
-  const [email,setEmail]=useState("")
-  const [name,setName]=useState("")
-  const [password,setPassword]=useState("")
-  const handlePassword = (e)=>{
-    console.log(e.target.value)
-    setPassword(e.target.value)
-  }
-  const handleName = (e)=>{
-    console.log(e.target.value)
-    setName(e.target.value)
-  }
-  const handleEmail = (e)=>{ 
-    console.log(e.target.value)
-    setEmail(e.target.value)
-  }
-  const handleSubmit= (e)=>{
-    e.preventDefault()
-    console.log(email ,password, name,);
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const handlePassword = (e) => {
+    console.log(e.target.value);
+    setPassword(e.target.value);
+  };
+  const handleName = (e) => {
+    console.log(e.target.value);
+    setName(e.target.value);
+  };
+  const handleEmail = (e) => {
+    console.log(e.target.value);
+    setEmail(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, name);
     axios({
-      method: 'post',
-      url: "http://localhost:3000/user/register", 
-      data: {email:email,password:password,name:name,gender:gender}
-    }).then(res=>{
-      console.log(res.data)
-    })
-  }
+      method: "post",
+      url: "http://localhost:3000/user/register",
+      data: { email: email, password: password, name: name, gender: gender },
+    }).then((res) => {
+      console.log(res.data);
+    });
+  };
   // const handleSubmit = (e)=>{
   //   e.preventDefault()
   //   axios({
   //     method: 'post',
-  //     url: "http://localhost:3000/login", 
+  //     url: "http://localhost:3000/login",
   //     data: {email:email,password:password}
   //   }).then(res=>{
   //     localStorage.setItem("token",res.data.token)
@@ -48,110 +50,111 @@ const UserSignup = () => {
     return inputValue.trim() !== "";
   };
   return (
-  
-    <div className='flex justify-center items-center  content-center '>
-      <form className='bg-slate-900 w-[30%] h-[62.5rem] p-7 grid content-center ' onSubmit={handleSubmit} >
-      <div className="relative ">
-                  <input
-                    id="name"
-                    type="name"
-                    name="name"
-                    className="border-b bg-transparent w-96 py-1 focus:outline-none focus:border-purple-600 focus:border-b-2 transition-colors peer"
-                    onChange={(e) => handleName(e)}
-                    value={name}
-                    required
-                  />
-                  <label
-                    htmlFor="name"
-                    className={`absolute left-0  text-gray-600 cursor-text transition-all ${
-                      isInputFilled(name)
-                        ? "bottom-[1.6rem] text-xs text-purple-600"
-                        : "peer-focus:text-xs peer-focus:-top-4 top-1 peer-focus:text-purple-600"
-                    }`}
-                  >
-                    Your Name
-                  </label>
-                </div>
-                <div className="relative mt-5">
-                  <input
-                    id="email"
-                    type="email"
-                    name="email"
-                    className="border-b bg-transparent w-96 py-1 focus:outline-none focus:border-purple-600 focus:border-b-2 transition-colors peer"
-                    autoComplete="off"
-                    onChange={(e)=>handleEmail(e)}
-                    value={email}
-                    required
-                  />
-                  <label
-                    htmlFor="name"
-                    className={`absolute left-0  text-gray-600 cursor-text transition-all ${
-                      isInputFilled(email)
-                        ? "bottom-[1.6rem] text-xs text-purple-600"
-                        : "peer-focus:text-xs peer-focus:-top-4 top-1 peer-focus:text-purple-600"
-                    }`}
-                  >
-                    Email
-                  </label>
-                </div>
-      <div className="relative mt-5">
-                  <input
-                    id="password"
-                    type="password"
-                    name="password"
-                    className="border-b bg-transparent w-96 py-1 focus:outline-none focus:border-purple-600 focus:border-b-2 transition-colors peer"
-                    autoComplete="off"
-                    onChange={(e)=>handlePassword(e)}
-                    value={password}
-                    required
-                  />
-                  <label
-                    htmlFor="name"
-                    className={`absolute left-0  text-gray-600 cursor-text transition-all ${
-                      isInputFilled(password)
-                        ? "bottom-[1.6rem] text-xs text-purple-600"
-                        : "peer-focus:text-xs peer-focus:-top-4 top-1 peer-focus:text-purple-600"
-                    }`}
-                  >
-                    Password
-                  </label>
-                </div>
-  
-    
-
-     
-<span>
-
-        <label htmlFor="">
-
-
-        <input
-          type="radio"
-          name="options"
-          value="Male"
-          checked={gender === 'Male'}
-          onChange={handleOptionChange}/>
-          male
-        </label>
-          <label>
+    <div className="flex justify-center items-center h-screen  content-center ">
+      <form
+        className="bg-slate-900 gap-4 w-[26vw] rounded-l-3xl h-[60vh] p-7 grid content-center "
+        onSubmit={handleSubmit}
+      >
+        <label className="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4 opacity-70"
+          >
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+          </svg>
           <input
-            type="radio"
-            name="options"
-            value="Female"
-            checked={gender === 'Female'}
-            onChange={handleOptionChange}
+            type="text"
+            name="name"
+            onChange={(e) => handleName(e)}
+            value={name}
+            className="grow"
+            placeholder="Username"
+            required
           />
-          Female
+        </label>
+
+        <label className="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4 opacity-70"
+          >
+            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+          </svg>
+          <input
+            type="text"
+            className="grow"
+            name="email"
+            value={email}
+            onChange={(e) => handleEmail(e)}
+            placeholder="Email"
+            required
+          />
+        </label>
+        <label className="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4 opacity-70"
+          >
+            <path
+              fillRule="evenodd"
+              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <input
+            type="password"
+            name="password"
+            onChange={(e) => handlePassword(e)}
+            className="grow"
+            value={password}
+          />
+        </label>
+
+        <span className="flex flex-col gap-2 ">
+          <label htmlFor="">
+            <input
+              type="radio"
+              name="options"
+              value="Male"
+              checked={gender === "Male"}
+              onChange={handleOptionChange}
+            />
+            male
           </label>
-</span>
-
-        <button className="btn btn-info" type='submit'>Submit</button>
-        
+          <label>
+            <input
+              type="radio"
+              name="options"
+              value="Female"
+              checked={gender === "Female"}
+              onChange={handleOptionChange}
+            />
+            Female
+          </label>
+        </span>
+        <button className="btn btn-info" type="submit">
+          Submit
+        </button>
+          <div className="flex gap-2 flex-col justify-center items-center">
+            <p>Aleady have an account?</p>
+            <Link className=" btn btn-outline  btn-accent w-[10vw] h-[3vh]  p-1 flex flex-row justify-center" to={"/login"} >Login</Link>
+          </div>
       </form>
-      <img className='w-[40%]' src={gym} alt="image of a dumbell
-      " />
+      <img
+        className="h-[60vh] rounded-r-3xl"
+        src={gym}
+        alt="image of a dumbell
+      "
+      />
     </div>
-  )
-}
+  );
+};
 
-export default UserSignup
+export default UserSignup;
--- fitness-tracker-frontend/src/pages/Userlogin.jsx
+++ fitness-tracker-frontend/src/pages/Userlogin.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
-import { ProtectedRoutes, ProtectedRouteProvider } from "../component/ProtectedRoutes.jsx";
+import { Link } from 'react-router-dom'
 
 const Userlogin = () => {
   const [email,setEmail]=useState("")
@@ -34,23 +34,28 @@ const Userlogin = () => {
 
   }
   return (
-    <div>
+    <div className='flex flex-col justify-center h-screen'>
+      <span className='flex  text-center items-center justify-center '>
+
+     <form className='bg-cyan-950 p-10 rounded-l-3xl h-[60.8vh] w-[20vw] flex flex-col justify-center' onSubmit={(e)=>handleSubmit(e)} >
       <h1>Log in </h1>
       <p>welcome back please enter your details </p>
-     <form onSubmit={(e)=>handleSubmit(e)} >
-     <label htmlFor="">
-        Email
-      </label>
-        <input type="text" value={email} placeholder='enter your email' name="email"
-          onChange={(e)=>handleEmail(e)}/>
-           <label htmlFor="">
-        Password
-      </label>
-        <input type="text" value={password}placeholder="password" onChange={(e)=>handlePassword(e)}
-        name="password"/>
+     <label className="input  input-bordered flex items-center gap-2">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+  <input type="text" value={email} className=" grow " placeholder="Enter your Email"  onChange={(e)=>handleEmail(e)} />
+</label>
+     
+        <label className="input mt-4 input-bordered flex items-center gap-2">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
+  <input type="password" className="grow" value={password}  onChange={(e)=>handlePassword(e)} />
+  </label>
+    
         <button type='submit'>Submit</button>
-        <p>dont have an account signup</p>
+        <p>Dont have an account </p>
+        <Link className='underline mt-4 border-4 w-1/2 btn btn-outline btn-accent' to="/signin">Sign Up</Link>
       </form>
+      <img className='w-[25vw] h-[60.8vh]  rounded-r-3xl' src="/LoginImage.jpg" alt="welcome to this bitch " />
+      </span>
     </div>
   )
 }
