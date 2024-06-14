90e7176bb0374da8ae876232e1f6bc5d737663cb
cejimenez0123 <christianjimenez0123@gmail.com>
2024-06-14 07:28:06 -0400

    hotfix

--- fitness-tracker-frontend/src/component/Execersize.jsx
+++ fitness-tracker-frontend/src/component/Execersize.jsx
@@ -5,7 +5,7 @@ import ExerciseTemplate from "./ExerciseTemplate";
 import Select from "react-select";
 import { v4 as uuidv4 } from "uuid";
 
-import SetTemplate from "./SetTemplate";
+import SetTemplate from "./setTemplate";
 const Execersize = ({
   setExerciseData,
   workout,
